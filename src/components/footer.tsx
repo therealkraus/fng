ae6d31b544592cec831a290cfd6b4abfda1c1063
Nikola Ilic <62488394+therealkraus@users.noreply.github.com>
2023-09-04 11:22:07 -0400

    formatted footer to add a hover effect to mail to button

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,19 @@
+import Link from "next/link";
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer>
       <div className="flex flex-col items-center justify-center ml-auto mr-auto sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl p-10">
-        <p className="font-semibold text-center text-neutral-400">
+        <p className="font-semibold text-center dark:text-neutral-400">
           Made with ❤️ by{" "}
-          <a
+          <Link
             href="mailto:nikolailic25@live.ca"
-            className="font-bold text-primary"
+            className="font-bold text-white hover:text-primary transition-all duration-500 ease-in-out hover:border-b hover:border-primary"
           >
             @nikolailic
-          </a>
+          </Link>
         </p>
-        <p className="text-center text-neutral-400">
+        <p className="text-center dark:text-neutral-400">
           &copy;{currentYear} Fantasy Name Creator
         </p>
       </div>
